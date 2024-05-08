cc42e6d187205d14cff0eebcf6600bdfb3a342a6
Stephen Strome <sStrome@hotmail.com>
2024-05-08 12:47:30 -0600

    Add touch detection on a resource object Make resource regenerate after five seconds Changed asset shapes to allow easier collision models

---
+++ scenes/main.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@export var max_resources = 6
+@export var starting_resources = 3
+var p1_resources = starting_resources
+var p2_resources = starting_resources
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_player_1_resource_gathered():
+	if p1_resources < max_resources:
+		p1_resources += 1
+	pass # Replace with function body.
---
+++ scenes/player.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+signal resource_gathered
+
+@export var speed = 400
+var screen_size
+var resources = 3
+@export var pos = 0
+@export var max_resources = 6
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	pass
+
+func start():
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _on_area_2d_body_entered(body):
+	print("test")
+	if body.name == "Resource":
+		
+		var point = body.resource_gathered()
+		print(point)
+		if point and resources < max_resources:
+			print("resource detected")
+			print(resources)
+			resources += 1
+	else:
+		print(body.name)
+	pass # Replace with function body.
